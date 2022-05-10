9730634ab8018803c4ce88d55bdb3018b3234639
LauraOrte <laura.orte7@gmail.com>
2022-05-10 23:43:51 +0200

    ok!

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -6,4 +6,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
+//poner las que no tengas nada
+
+
 module.exports = router;
